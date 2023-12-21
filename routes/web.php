e2448ae694ef2251b8864456dd5bc77244b998cd
ibrahim Mohammed <kxibrahimxk@gmail.com>
2023-12-22 01:28:08 +0300

    add admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin/index');
+});
+
+Route::get('/Wallet', function () {
+    return view('admin/tables_data');
+});
+
+Route::get('/forms-validation', function () {
+    return view('admin/forms-validation');
 });
