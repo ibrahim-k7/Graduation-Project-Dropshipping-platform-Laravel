3d9a8112d00b9a09780b4a0d6e99ce21d30e31b4
alshami <moha47209@gmail.com>
2024-01-20 22:50:53 +0300

    لياايا

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,15 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    
+
     //الجدول المربوط به
     protected $table = "products";
     //العناصر
-    protected $fillable = [
+    protected $fillable = [0
         'id','cat_id','subCat_id','name','description','purchasing_price','selling_price','suggested_selling_price','weight','quantity','barcode','image','created_at','updated_at'
     ];
 
-    public function purchases(){
+    public function purchase(){
         return $this-> belongsToMany(Purchase::class,'purchase details','pro_id','purch_id','id','id');
     }
 }
