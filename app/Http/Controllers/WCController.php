5f89617a7fee6b8065db4e45d4aeae4d07904923
Mwadei <97555759+Mwadei@users.noreply.github.com>
2024-03-07 05:34:21 +0300

    shipping methods API

--- app/Http/Controllers/WCController.php
+++ app/Http/Controllers/WCController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
+use App\Models\Delivery;
 use Automattic\WooCommerce\Client;
 use Illuminate\Http\Request;
 class WCController extends Controller
@@ -84,5 +85,38 @@ class WCController extends Controller
         }
     }
 
-    //
+    //دالة تقوم بتضمين الموصل في الwoocommerce
+    public function inlcudeShippingMethod($name,$shipping_fees){
+        $woocommerce = new Client(
+            'https://m5zndrop.online/',
+            'ck_5fac83320f432d1efc44e8a16f4d30eaa916f4c0',
+            'cs_e60d57a56819c9170c50d90ba54234d67344381a',
+            [
+                'version' => 'wc/v3',
+            ]
+        );
+
+        $data = [
+            'method_id' => 'free_shipping',
+            'settings' => [
+                'title' => $name,
+                'cost' => $shipping_fees,
+            ]
+        ];
+
+        print_r($woocommerce->post('shipping/zones/1/methods', $data));
+    }
+
+    //دالة تقوم باستدعاء جميع الموصلين لتضمينهم في woocommerce
+    public function getDelivery(){
+
+        $deliveries = Delivery::select('*')->get();
+
+        foreach ($deliveries as $delivery){
+            $this->inlcudeShippingMethod($delivery->name,$delivery->shipping_fees);
+        }
+
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //WC API
 Route::post('/send_product_to_WC',[WCController::class , 'linkProduct'])->name('WC.API.link.product');
 Route::delete('/unlink_product_from_WC',[WCController::class,'unlinkProduct'])->name('WC.API.unlink.products');
+Route::get('/get_delivery',[WCController::class,'getDelivery'])->name('WC.API.get.delivery');
